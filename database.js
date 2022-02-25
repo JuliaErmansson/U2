a964c7b0f707d4065fd5ddeb6e424543a7c72f76
Julia Ermansson <ermanssonj@gmail.com>
2022-02-25 13:56:16 +0100

    Database is done
    My array database is done

---
+++ database.js
@@ -0,0 +1,73 @@
+let database = [
+    {
+        Id: 1,
+        Song: "Highway to hell",
+        Artist: "Acdc",
+        ReleaseYear:1979,
+        Genre: "Hard Rock",
+
+    },
+    {
+        Id: 2,
+        Song: "Woman",
+        Artist: "Doja Cat",
+        ReleaseYear: 2021,
+        Genre: "Afrobeat",
+    },
+    {
+        Id: 3,
+        Song: "Holoscene",
+        Artist: "Bon Iver",
+        ReleaseYear: 2011,
+        Genre: "Indie Folk",
+    },
+    {
+        Id: 4,
+        Song: "Passa dig",
+        Artist: "Miriam Bryant",
+        ReleaseYear:2021,
+        Genre: "Pop",
+    },
+    {
+        Id: 5,
+        Song: "The Dahmers",
+        Artist: "Nightcrawler",
+        ReleaseYear:2017,
+        Genre: "Garage Punk",
+    },
+    {
+        Id: 6,
+        Song: "Bad Guy",
+        Artist: "Billie Eilish",
+        ReleaseYear:2019,
+        Genre: "Electropop",
+    },{
+        Id: 7,
+        Song: "Lose Yourself",
+        Artist: "Eminem",
+        ReleaseYear:2002,
+        Genre: "Hip hop",
+        
+    },
+    {
+        Id: 8,
+        Song: "Hello",
+        Artist: "Adele",
+        ReleaseYear:2015,
+        Genre: "Soul",
+    },
+    {
+        Id: 9,
+        Song: "Rehab",
+        Artist: "Amy Winehouse",
+        ReleaseYear:2006,
+        Genre: "Soul",
+    },
+    {
+        Id: 10,
+        Song: "This Is America",
+        Artist: "Donald Glover",
+        ReleaseYear:2018,
+        Genre: "Afrobeat",
+    },
+]
